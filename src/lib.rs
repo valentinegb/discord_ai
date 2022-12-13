e61068e756638100d5fefe36da4d4284545f8fc6
Valentine <valentinegb@icloud.com>
2022-12-12 19:52:17 -0800

    Outline project structure

---
+++ src/lib.rs
@@ -0,0 +1,2 @@
+// This is where the AI code goes.
+// It's like the "backend".
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+// This is where the Discord bot code goes, the interface between the AI and the users.
+// It's like the "frontend".
+
 fn main() {
-    println!("Hello, world!");
+    todo!();
 }
