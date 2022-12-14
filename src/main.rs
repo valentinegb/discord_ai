09affedad0173dc460b07bf2a78ee57d6ed32cd7
Valentine <valentinegb@icloud.com>
2022-12-14 11:46:16 -0800

    Get basic bot code working

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
 // This is where the Discord bot code goes, the interface between the AI and the users.
 // It's like the "frontend".
 
-fn main() {
-    todo!();
+use dotenv::dotenv;
+use std::env;
+use serenity::{ client::Client, prelude::GatewayIntents };
+
+// TODO: Gateway event handler
+
+#[tokio::main]
+async fn main() {
+    dotenv().expect("Failed to load .env file");
+
+    let mut client = Client::builder(
+        env::var("DISCORD_BOT_TOKEN").expect("DISCORD_BOT_TOKEN env var not found"),
+        GatewayIntents::empty(),
+    ).await.expect("Failed to build client");
+
+    // Print error without panicking
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
 }
